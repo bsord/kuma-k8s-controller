e4fafa73e890b0a205d5f68b4cf51eda3a2cc8b7
mgmtadmin <brandon.sorgdrager@gmail.com>
2021-08-11 17:10:08 -0500

    Better error handling and refactor

--- client/run.go
+++ client/run.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/spf13/cobra"
-	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -46,13 +45,20 @@ func run(cmd *cobra.Command, args []string) {
 	}
 
 	// Initiate informer factory
-	informerFactory := informers.NewSharedInformerFactory(clientset, 10*time.Minute)
+	informerFactory := informers.NewSharedInformerFactory(clientset, 0)
 
 	// create ingress informer inside the factory
-	ingressInformer := informerFactory.Networking().V1().Ingresses()
+	ingressInformer := informerFactory.Networking().V1().Ingresses().Informer()
+
+	// Create a channel to stop the shared informer gracefully
+	stopper := make(chan struct{})
+	defer close(stopper)
+
+	// Kubernetes serves an utility to handle API crashes
+	defer runtime.HandleCrash()
 
 	// define function handlers for ingress informer
-	ingressInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+	ingressInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(new interface{}) {
 			fmt.Println("ingress was added")
 		},
@@ -64,15 +70,15 @@ func run(cmd *cobra.Command, args []string) {
 		},
 	})
 
-	// start the informers
-	informerFactory.Start(wait.NeverStop)
-	informerFactory.WaitForCacheSync(wait.NeverStop)
+	fmt.Println("test")
 
-	// list the ingresses in the informer.
-	ingress, err := ingressInformer.Lister().Ingresses("").Get("")
-	if err != nil {
-		fmt.Println("there was an error listing the ingresses")
+	// Run the informer
+	ingressInformer.Run(stopper)
+
+	// Handle cache sync failure.
+	if !cache.WaitForCacheSync(stopper, ingressInformer.HasSynced) {
+		runtime.HandleError(fmt.Errorf("timed out waiting for caches to sync"))
+		return
 	}
-	fmt.Println(ingress)
 
 }
